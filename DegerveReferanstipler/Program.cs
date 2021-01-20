752d550d5fcf4a9aa150aaa356672d4d40a92f4d
Berke Can Bal <berkebaltr@hotmail.com>
2021-01-20 23:50:37 +0300

    20 Ocak ders sonu

--- DegerveReferanstipler/Program.cs
+++ DegerveReferanstipler/Program.cs
@@ -6,7 +6,6 @@ namespace DegerveReferanstipler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
             int sayi1 = 10;
             int sayi2 = 30;
